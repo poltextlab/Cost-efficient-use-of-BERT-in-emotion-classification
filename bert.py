24da01ed9f39916207486fe7da77e769c44ab455
György Márk Kis <kis.gyorgy.mark@gmail.com>
2021-11-02 10:05:55 +0100

    Update bert.py

--- bert.py
+++ bert.py
@@ -9,7 +9,11 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import torch
 from transformers import AutoTokenizer, AutoModel
-
+import json
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 import pandas as pd
 import numpy as np
 import os
@@ -162,7 +166,40 @@ def main():
             print(f'#{i} L{al} {issamemark} #{j} L{bl} = {dist} {anom}')
 
         # XXX add clustering at the end -> polysemy will be solved :)
+    # MinMax scaling is applied to the features
+    scaler = MinMaxScaler()
+    featuresfinal = scaler.fit_transform(featuresfinal)
+    
+    # the parameter space is defined below
+    C = [0.1, 1]
+    tol = [0.001, 0.005, 0.01]
+    weighting = ['balanced']
+    solver = ['liblinear']
+    max_iter = [8000]
+    parameters = dict(C=C, tol=tol, class_weight=weighting, solver=solver, max_iter=max_iter)
 
+    clasrep = list()
+    paramlist = list()
+    labels = corpus["topik"].to_numpy()
+    
+    for i in range(3):
+        train_features, test_features, train_labels, test_labels = train_test_split(featuresfinal, labels, stratify=labels)
+        lr = LogisticRegression()
+        lrmodel = GridSearchCV(lr, parameters, cv = 3, scoring = 'f1_weighted', n_jobs = -1)
+        lrmodel.fit(train_features, train_labels)
+        predictions = lrmodel.predict(test_features)
+        classifrep = classification_report(test_labels, predictions, output_dict = True)
+        clasrep.append(classifrep)
+        paramlist.append(lrmodel.best_params_)
+        print("Finished with run!")
+        
+    MyFile = open('clasrep_bert.json', 'w')
+    json.dump(clasrep, MyFile)
+    MyFile.close()
+
+    MyFile = open('param_bert.json', 'w')
+    json.dump(paramlist, MyFile)
+    MyFile.close()
 
 def get_args():
     """Parse command line arguments."""
